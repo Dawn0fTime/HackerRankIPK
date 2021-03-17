a07af65b41c192de9617052f09e1431a9a24b51d
Dawn C. DeMeo <Dawn.DeMeo@gmail.com>
2021-03-17 10:00:35 -0400

    Initial commit

---
+++ src/main/java/com/dawndemeo/Solution.java
@@ -0,0 +1,18 @@
+package com.dawndemeo;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/com/dawndemeo/SolutionTest.java
@@ -0,0 +1,19 @@
+package com.dawndemeo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void failTest() {
+        fail();
+    }
+
+    @Test
+    public void passTest() {
+        assertTrue(true);
+    }
+
+}
