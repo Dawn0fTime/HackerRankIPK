719fdd71920ad8a52279e019f774f3f91caa167c
Dawn C. DeMeo <Dawn.DeMeo@gmail.com>
2021-03-17 10:33:44 -0400

    2D Array - DS completed.

--- src/test/java/com/dawndemeo/SolutionTest.java
+++ src/test/java/com/dawndemeo/SolutionTest.java
@@ -2,18 +2,38 @@ package com.dawndemeo;
 
 import org.junit.Test;
 
+import static com.dawndemeo.Solution.hourglassSum;
 import static org.junit.Assert.*;
 
 public class SolutionTest {
 
     @Test
-    public void failTest() {
-        fail();
+    public void example() {
+        int[][] arr = {{-9, -9, -9, 1, 1, 1},
+                {0, -9, 0, 4, 3, 2},
+                {-9, -9, -9, 1, 2, 3},
+                {0, 0, 8, 6, 6, 0},
+                {0, 0, 0, -2, 0, 0},
+                {0, 0, 1, 2, 4, 0}};
+
+        int expectedResult = 28;
+
+        assertEquals(expectedResult, hourglassSum(arr));
     }
 
     @Test
-    public void passTest() {
-        assertTrue(true);
+    public void sample() {
+        int[][] arr = {{1, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 0, 0, 0},
+                {0, 0, 2, 4, 4, 0},
+                {0, 0, 0, 2, 0, 0},
+                {0, 0, 1, 2, 4, 0}};
+
+        int expectedResult = 19;
+
+        assertEquals(expectedResult, hourglassSum(arr));
     }
+
 
 }
